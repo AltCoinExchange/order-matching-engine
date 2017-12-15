7126860b42a8edd30011f05112c92110b6fdb0c1
MisterDr <djenadrazic@gmail.com>
2017-12-15 11:55:57 +0100

    - Added logging middleware

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,11 +1,19 @@
-import {Module} from "@nestjs/common";
+import {MiddlewaresConsumer, Module, NestModule, RequestMethod} from "@nestjs/common";
 import {AppController} from "./app.controller";
 import {RatesController} from "./controllers/rates.controller";
 import {DataController} from "./controllers/data.controller";
+import {LoggerMiddleware} from "./middleware/logger.middleware";
 
 @Module({
   modules: [],
   controllers: [AppController, RatesController, DataController],
   components: [],
 })
-export class ApplicationModule {}
+
+export class ApplicationModule implements NestModule {
+    public configure(consumer: MiddlewaresConsumer): void {
+        consumer.apply(LoggerMiddleware).forRoutes(
+            RatesController, DataController, // { path: "/transaction", method: RequestMethod.GET },
+        );
+    }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from "@nestjs/core";
 import { ApplicationModule } from "./modules/app.module";
+import * as clicolor from "cli-color";
 
 async function bootstrap() {
     const app = await NestFactory.create(ApplicationModule);
+    // tslint:disable-next-line
+    console.log(clicolor.yellow("Order Matching Engine Listening on port: " + 3000));
     await app.listen(3000);
 }
 bootstrap();
