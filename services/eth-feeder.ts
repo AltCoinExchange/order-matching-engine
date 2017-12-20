91b0da98d0e97bc8045bf505bf2f87db1274cde8
MisterDr <djenadrazic@gmail.com>
2017-12-20 01:46:41 +0100

    - Removed unneded references

--- services/eth-feeder.ts
+++ services/eth-feeder.ts
@@ -1,16 +1,17 @@
 import {FeederService} from "./common/FeederService";
-import {EthEngine} from "altcoinio-wallet";
+import {EthEngine} from "../wallet/src/eth";
 import {EthConfiguration} from "./config/eth";
 import "core-js/es6/reflect";
 import "core-js/es7/reflect";
+import "reflect-metadata";
 
-declare abstract class Reflect {
-    public static getMetadata(metadataKey: any, target: Object, targetKey: string | symbol): any;
-    public static getOwnMetadata(metadataKey: any, target: Object, targetKey: string | symbol): any;
-    public static getOwnMetadata(metadataKey: any, target: Object): any;
-    public static defineMetadata(metadataKey: any, metadataValue: any,
-                                 target: Object, targetKey?: string | symbol): void;
-}
+// declare abstract class Reflect {
+//     public static getMetadata(metadataKey: any, target: Object, targetKey: string | symbol): any;
+//     public static getOwnMetadata(metadataKey: any, target: Object, targetKey: string | symbol): any;
+//     public static getOwnMetadata(metadataKey: any, target: Object): any;
+//     public static defineMetadata(metadataKey: any, metadataValue: any,
+//                                  target: Object, targetKey?: string | symbol): void;
+// }
 
 export class EthFeeder extends FeederService {
     public ethEngine: EthEngine;
