08ce25409518e73a2725189a196be9461200a103
Ubuntu <ubuntu@ip-172-31-35-200.us-west-2.compute.internal>
2017-12-28 16:31:20 +0000

    - Added certs

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,4 @@
 export const Config = {
-  SSL_KEY_PATH: "",
-  SSL_CERT_PATH: "",
+  SSL_KEY_PATH: "/etc/letsencrypt/live/altcoin.ddns.net/privkey.pem",
+  SSL_CERT_PATH: "/etc/letsencrypt/live/altcoin.ddns.net/fullchain.pem",
 };
