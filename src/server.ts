9c3d03716e435ea081fcfe6e817d31e66ecf07f5
MisterDr <djenadrazic@gmail.com>
2018-01-18 23:34:52 +0100

    - Reverted headers change

--- src/server.ts
+++ src/server.ts
@@ -35,14 +35,12 @@ async function bootstrap(serveHttps: boolean = false) {
     console.log(clicolor.yellow("Order Matching Engine Listening on HTTPS port: " + 3000));
   } else {
     const expressApp = require("express")();
+    expressApp.use((req, res, next) => {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      next();
+    });
 
-    if (process.env.NODE_ENV === undefined) {
-      expressApp.use((req, res, next) => {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        next();
-      });
-    }
     const app = await NestFactory.create(ApplicationModule, expressApp);
     // app.useWebSocketAdapter(new WsAdapter(3001));
 
