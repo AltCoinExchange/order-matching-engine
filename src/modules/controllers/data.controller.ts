c4a538a825065076d70f800a77e82d2e2e642ed8
MisterDr <djenadrazic@gmail.com>
2018-01-29 19:06:47 +0100

    - Added confirmations

--- src/modules/controllers/data.controller.ts
+++ src/modules/controllers/data.controller.ts
@@ -1,6 +1,8 @@
 import {Get, Controller, Param} from "@nestjs/common";
 import * as Mongoose from "mongoose";
 import {Collection} from "mongoose";
+import {EthConfiguration} from "../../../services/config/eth";
+import * as Web3 from "web3/src";
 
 @Controller("data")
 export class DataController {
@@ -14,6 +16,10 @@ export class DataController {
     await Mongoose.connect("mongodb://127.0.0.1:27017/eth", {useMongoClient: true});
     const coll = Mongoose.connection.collection("eth");
 
+    const wsProvider = new Web3.providers.WebsocketProvider(EthConfiguration.hosts[0].wshost);
+    const web3 = new Web3(wsProvider);
+    const blockNumber = await web3.eth.getBlockNumber();
+
     const transactions = {} as any;
     // TODO: Probably this should occur at engine to not pass : into parameters
     params.address = params.address.slice(1).toLowerCase();
@@ -25,6 +31,7 @@ export class DataController {
       for (const i in from.transactions) {
         if (from.transactions[i].from === params.address) {
           from.transactions[i].timestamp = from.timestamp;
+          from.transactions[i].confirmations = blockNumber - from.transactions[i].blockNumber;
           transactions.from.push(from.transactions[i]);
         }
       }
@@ -35,6 +42,7 @@ export class DataController {
       for (const i in to.transactions) {
         if (to.transactions[i].to === params.address) {
           to.transactions[i].timestamp = to.timestamp;
+          to.transactions[i].confirmations = blockNumber - to.transactions[i].blockNumber;
           transactions.from.push(to.transactions[i]);
         }
       }
