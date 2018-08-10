e697637b893f903e31adb7aee9791d6de6b04622
MisterDr <djenadrazic@gmail.com>
2018-08-10 16:29:20 +0200

    - Fix for the wss connection

--- src/modules/controllers/faucet.controller.ts
+++ src/modules/controllers/faucet.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get, Param } from "@nestjs/common";
 import { EthereumWallet } from "altcoinio-wallet";
-import { WalletFactory } from "../../../bot/common/wallet/WalletFactory";
 import { AppConfig } from "../../../bot/config/app";
 
 @Controller("faucet")
@@ -8,20 +7,22 @@ export class FaucetController {
   @Get("fund/:address")
   public async faucet(@Param() params): Promise<any> {
     const walletAddress = AppConfig.faucetAddress;
-    const ethWallet = new EthereumWallet("testnet");
-    return ethWallet.getbalance(walletAddress).then((result) => {
-      if (result < 10) {
-        return {err: "Not enough!"};
-      }
+    let ethWallet = new EthereumWallet("testnet");
 
-      return ethWallet.getbalance(params.address).then((addressBalance) => {
-        if (addressBalance > 0) {
-          return {err: "Address already has funds!"};
-        }
-        const acc = ethWallet.recover(AppConfig.faucetSeed);
-        ethWallet.login(acc);
-        return ethWallet.sendEther(params.address, "0.02");
-      });
-    });
+    const balance = await ethWallet.getbalance(walletAddress);
+    if (balance < 10) {
+      return {err: "Not enough!"};
+    }
+
+    const fundedBalance = await ethWallet.getbalance(params.address);
+    if (fundedBalance > 0) {
+      return {err: "Address already has funds!"};
+    }
+
+    const acc = ethWallet.recover(AppConfig.faucetSeed);
+    ethWallet.login(acc);
+    const result = await ethWallet.sendEther(params.address, "0.02");
+    ethWallet = null;
+    return result;
   }
 }
