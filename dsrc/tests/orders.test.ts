7fdeebcf4b6f1995101a17878254b34b07b62d36
MisterDr <djenadrazic@gmail.com>
2018-08-17 14:31:30 +0200

    - Test fix

--- dsrc/tests/orders.test.ts
+++ dsrc/tests/orders.test.ts
@@ -1,6 +1,5 @@
-import {WhisperService} from "../../bot/common/clients/Whisper";
 import {EthConfiguration} from "../../services/config/eth";
-import {OrbitDbProvider} from '../db/OrbitDbProvider';
+import {OrbitDbProvider} from '../../library/db/OrbitDbProvider';
 
 async function bootstrap() {
   const db = new OrbitDbProvider();
